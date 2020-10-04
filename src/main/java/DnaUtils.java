f202776370a20f59c4cf1c02f902dda99ed84286
Prof. Diogo S. Martins <santana.martins@ufabc.edu.br>
2020-10-04 17:25:50 -0300

    removed accents

--- src/main/java/DnaUtils.java
+++ src/main/java/DnaUtils.java
@@ -8,7 +8,7 @@ public final class DnaUtils {
     /**
      * Transforma a string de entrada (no formato de sequencia DNA) no complemento
      * de Watson-Crick, isto eh, substitui A por T e C por G, e vice versa.
-     * @param dna a sequência DNA formada por uma sequencia de A, C, G ou T
+     * @param dna a sequencia DNA formada por uma sequencia de A, C, G ou T
      * @return o complemento de Watson-Crick para a sequencia
      */
     public static String watsonCrick(String dna) {
